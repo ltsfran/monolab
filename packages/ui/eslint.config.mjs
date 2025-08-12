134376d65199096c105d22e5a18cbe4e650f73fb
Luis Tupa <luistupa6@gmail.com>
2025-08-12 18:13:03 -0500

    chore: update ESLint configurations and improve code formatting

--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -1,4 +1,4 @@
-import { config } from "@monolab/eslint-config/react-internal";
+import { config } from '@monolab/eslint-config/react-internal'
 
 /** @type {import("eslint").Linter.Config} */
-export default config;
+export default config
