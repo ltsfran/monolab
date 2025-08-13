0d6348b493a449c51b88653b63580c42f3a8a9f3
Luis Tupa <luistupa6@gmail.com>
2025-08-13 15:29:51 -0500

    feat: add test script to package.json and configure Turbo for testing

---
+++ examples/with-next-vitest/src/app/__tests__/page.spec.tsx
@@ -0,0 +1,16 @@
+import Home from '@/app/page'
+import { render, screen } from '@testing-library/react'
+import { describe, expect, test } from 'vitest'
+
+describe('Home component', () => {
+  test('renders the main headline elements', () => {
+    render(<Home />)
+
+    expect(screen.getByAltText('Next.js logo')).toBeInTheDocument()
+
+    expect(screen.getByText(/Get started by editing/i)).toBeInTheDocument()
+    expect(
+      screen.getByText(/Save and see your changes instantly./i)
+    ).toBeInTheDocument()
+  })
+})
